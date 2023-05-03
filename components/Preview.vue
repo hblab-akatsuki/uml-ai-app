0125d6bff76254225c7fefe1ad6fc5058b639114
tiennt <tokentien@gmail.com>
2023-05-03 20:52:34 +0700

    feat: allow https

--- components/Preview.vue
+++ components/Preview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="previewSvg">
     <div class="download-png">
-      <button v-on:click="download" 
+      <button v-on:click="download"
         class="download mr-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
         <i class="fal fa-download mr-1"></i>
         SVG
@@ -28,7 +28,7 @@ export default {
     this.getSvgData(this.codeUml)
     const element = this.$refs.myElement
     this.$panzoom(element, {
-      minZoom: 0.5, 
+      minZoom: 0.5,
       maxZoom: 1.25
     })
 
@@ -47,7 +47,7 @@ export default {
   methods: {
     async getSvgData(text, type = 'svg') {
       const encode = this.encode(text || '');
-      const url = `http://www.plantuml.com/plantuml/${type}/${encode}`;
+      const url = `https://www.plantuml.com/plantuml/${type}/${encode}`;
       const response = await fetch(url)
       this.html = await response.text()
     },
@@ -55,9 +55,9 @@ export default {
       return plantumlEncoder.encode(text);
     },
     async download() {
-      const svgString = document.querySelector('.previewSvg svg').outerHTML; 
+      const svgString = document.querySelector('.previewSvg svg').outerHTML;
       const blob = new Blob([svgString], { type: 'image/svg+xml' });
-      saveAs(blob, 'my-svg-file.svg'); 
+      saveAs(blob, 'my-svg-file.svg');
     }
   },
   updated() {
@@ -67,7 +67,7 @@ export default {
 
     const element = this.$refs.myElement
     this.$panzoom(element, {
-      minZoom: 0.5, 
+      minZoom: 0.5,
       maxZoom: 1.25
     })
   },
@@ -81,7 +81,7 @@ export default {
   display: flex;
   justify-content: flex-end;
 }
-.download { 
+.download {
   margin-right: 20px;
   margin-top: 10px;
 }
